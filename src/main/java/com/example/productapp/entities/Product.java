a3269d5ea0779660fabff719692d2124f67da619
OUTLOU IMANE <outlouimane30@email.com>
2025-04-24 12:40:06 +0100

    first commit

--- src/main/java/com/example/productapp/entities/Product.java
+++ src/main/java/com/example/productapp/entities/Product.java
@@ -1,23 +1,31 @@
 package com.example.productapp.entities;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Product {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
 
-    private double price;
+    // Constructeur vide obligatoire pour JPA
+    public Product() {}
 
-    private int quantity;
+    // Getters et setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
